36bf3e9faa50adc29399b9063e5125d99ddc7e30
akaya1023 <an7099265@gmail.com>
2023-07-05 23:58:47 +0900

    New

---
+++ force-app/main/default/lwc/campaignMemberDetails/campaignMemberDetails.js
@@ -0,0 +1,32 @@
+import { LightningElement, api,track,wire } from 'lwc';
+import getCampaignMemberRecord from '@salesforce/apex/CamaignMemberDetailsController.getCampaignMemberRecord';
+
+
+export default class campaignMemberDetails extends LightningElement {
+    @api recordId; // Task record's ID
+    FirstName;
+
+    @wire(getCampaignMemberRecord, { taskId: '$recordId' })
+    campaignMemberData
+    /* campaignMemberData(data,error){
+        if(data){
+            this.FirstName = data.FirstName;
+        }else if(error){
+            console.error("error", error);
+        }
+    } */
+    /* campaignMemberData; */
+
+    handleClick() {
+        console.log("check");
+        console.log("record Id: " + this.recordId);
+    
+        if (this.campaignMemberData && this.campaignMemberData.data) {
+            console.log("campaign Member Data: " + JSON.stringify(this.campaignMemberData.data));
+        } else {
+            console.log("Campaign Member Data is not available yet.");
+        }
+    }
+
+}
+
